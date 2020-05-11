a1e0a904a94a3d6634b289561468c38d1a0a11d7
jayagajjar <62688276+jayagajjar@users.noreply.github.com>
2020-05-11 22:20:33 +1000

    mongodb env variable added

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/kilogramsDB', { useNewUrlParser: true ,useUnifiedTopology: true },)
+    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kilogramsDB', { useNewUrlParser: true ,useUnifiedTopology: true },)
     .catch(e => {
         console.error('Connection error', e.message)
     })
