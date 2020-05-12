921fe0b36a19d341a6cbd3d55b95262d574684aa
jayagajjar <62688276+jayagajjar@users.noreply.github.com>
2020-05-12 23:05:11 +1000

    process.env.PORT added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const itemRouter = require('./routes/item-router')
 const shoppinglistRouter = require('./routes/shoppinglist-router')
 
 const app = express()   
-const apiPort = 3000
+const apiPort = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
