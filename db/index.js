34a2bec3397182b2cc2a7a72e22bc7e89d3697ae
Jaya <jaya.wired@gmail.com>
2020-08-26 15:09:20 +1000

    Database migrated from mLab to atlas

--- db/index.js
+++ db/index.js
@@ -4,14 +4,14 @@ const mongoose = require('mongoose')
 //    .connect('mongodb://heroku_vvz3nlgr:qiepu79g2s6aqnjcig70rgn413@ds111063.mlab.com:11063/heroku_vvz3nlgr' , { useNewUrlParser: true ,useUnifiedTopology: true },)
 
 const connection = mongoose
-    .connect('mongodb://heroku_vvz3nlgr:qiepu79g2s6aqnjcig70rgn413@ds111063.mlab.com:11063/heroku_vvz3nlgr' , { useNewUrlParser: true ,useUnifiedTopology: true },)
-
+    //.connect('mongodb://heroku_vvz3nlgr:qiepu79g2s6aqnjcig70rgn413@ds111063.mlab.com:11063/heroku_vvz3nlgr' , { useNewUrlParser: true ,useUnifiedTopology: true },)
+    //.connect('mongodb://127.0.0.1:27017/kilogramsDB' , { useNewUrlParser: true ,useUnifiedTopology: true },)
+    .connect('mongodb+srv://admin:admin@cluster0.ppebl.mongodb.net/kilograms?retryWrites=true&w=majority', { useNewUrlParser: true ,useUnifiedTopology: true },)
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
  
 const db = mongoose.connection
-
 
 module.exports = db
